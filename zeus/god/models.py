507a666370821b1ad9decf910a329773a1d09fcf
nightwarrior-xxx <amanv1999@gmail.com>
2019-11-28 04:34:18 +0530

    [Added] Guest Checkout functionality

--- zeus/god/models.py
+++ zeus/god/models.py
@@ -20,6 +20,7 @@ class Client(models.Model):
     checkInTime = models.DateTimeField(auto_now_add=True)
     checkOutTime = models.DateTimeField(auto_now=True)
     email = models.EmailField()
+    inMeeting = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
